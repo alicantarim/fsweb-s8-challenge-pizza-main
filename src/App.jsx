4803b87ff79f234018ee0a454c12a5980c77d5ea
alicantarim <alicantarim@hotmail.com>
2024-11-05 15:41:03 +0300

    edits app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import { useState } from "react";
-// import reactLogo from './assets/react.svg'
-// import workintech from '/workintech.svg'
+import reactLogo from "./assets/react.svg";
+import workintech from "/workintech.svg";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div className="App">
-        <h1>Pokemon Card Game</h1>
-      </div>
-    </>
+    <div className="App">
+      <header></header>
+    </div>
   );
 }
 
