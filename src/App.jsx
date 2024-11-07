baf55d1cae4b92bd862f832390cfbf5b324a1085
alicantarim <alicantarim@hotmail.com>
2024-11-07 15:18:00 +0300

    creates setting css

--- src/App.jsx
+++ src/App.jsx
@@ -1,98 +1,74 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import workintech from "/workintech.svg";
-import Extra from "./Extra";
+// import reactLogo from "./assets/react.svg";
+// import workintech from "/workintech.svg";
+// import Extra from "./Extra";
 
-const init = [{ name: "İnce" }, { name: "Normal" }, { name: "Kalın" }];
-
-const extras = [
-  {
-    value: "pepperoni",
-    label: "Pepperoni",
-  },
-  {
-    value: "sosis",
-    label: "Sosis",
-  },
-  {
-    value: "kanadaJambonu",
-    label: "Kanada Jambonu",
-  },
-  {
-    value: "tavukIzgara",
-    label: "Tavuk Izgara",
-  },
-  {
-    value: "sogan",
-    label: "Soğan",
-  },
-  {
-    value: "domates",
-    label: "Domates",
-  },
-  {
-    value: "misir",
-    label: "Mısır",
-  },
-  {
-    value: "sucuk",
-    label: "Sucuk",
-  },
-  {
-    value: "jalepeno",
-    label: "Jalepeno",
-  },
-  {
-    value: "sarimsak",
-    label: "Sarımsak",
-  },
-  {
-    value: "biber",
-    label: "Biber",
-  },
-  {
-    value: "zeytin",
-    label: "Zeytin",
-  },
-  {
-    value: "ananas",
-    label: "Ananas",
-  },
-  {
-    value: "kabak",
-    label: "Kabak",
-  },
+const pizzaSizes = [
+  { id: "s", size: "Küçük" },
+  { id: "m", size: "Orta" },
+  { id: "l", size: "Büyük" },
 ];
 
-const initialData = {
-  isim: "string",
-  boyut: "string",
-  malzemeler: [],
-  özel: "string",
-};
+const doughTypes = [
+  { id: "i", size: "İnce" },
+  { id: "n", size: "Normal" },
+  { id: "k", size: "Kalın" },
+];
+
+const extras = [
+  { id: 1, name: "Pepperoni" },
+  { id: 2, name: "Domates" },
+  { id: 3, name: "Biber" },
+  { id: 4, name: "Sosis" },
+  { id: 5, name: "Mısır" },
+  { id: 6, name: "Sucuk" },
+  { id: 7, name: "Kanada Jambonu" },
+  { id: 8, name: "Ananas" },
+  { id: 9, name: "Tavuk Izgara" },
+  { id: 10, name: "Jalepeno" },
+  { id: 11, name: "Kabak" },
+  { id: 12, name: "Soğan" },
+  { id: 13, name: "Sarımsak" },
+];
+
+const price = 85.5;
 
 function App() {
-  const [formData, setFormData] = useState(initialData);
+  // const [formData, setFormData] = useState(initialData);
+  const [count, setCount] = useState(1);
+  const [totalPrice, setTotalPrice] = useState(price * count);
+  const [elections, setElections] = useState([]);
 
-  function handleChange(event) {
-    const { type, name, value, checked } = event.target;
-    console.log(type, name, value, checked);
+  const increment = (event) => {
+    event.preventDefault();
+    setCount(count + 1);
+  };
 
-    if (type === "checkbox" && name === "malzemeler") {
-      setFormData((prevData) => ({
-        ...prevData,
-        malzemeler: checked
-          ? [...prevData.malzemeler, value]
-          : prevData.malzemeler.filter((item) => item !== value),
-      }));
+  const decrement = (event) => {
+    event.preventDefault();
+    count > 1 ? setCount(count - 1) : setCount(1);
+  };
+
+  const addElection = (event) => {
+    const { value, checked } = event.target;
+
+    if (checked) {
+      // Checkbox işaretlenirse değeri "elections" array e ekler.
+      setElections((prevElections) => [...prevElections, value]);
     } else {
-      setFormData({
-        ...formData,
-        [name]: value,
-      });
+      // Checkbox işareti kaldırılırsa, array'den çıkarır.
+      setElections((prevElections) =>
+        prevElections.filter((item) => item !== value)
+      );
     }
-  }
+  };
 
+  const handleOrderCompleted = (event) => {
+    event.preventDefault();
+    alert("Sipariş Verildi");
+  };
+
+  // UI SCREEN SECTION START HERE
   return (
     <div className="App">
       <header>
@@ -130,23 +106,24 @@ function App() {
 
         <div className="">
           {/* ORDER SIZE SECTION START */}
-          <div className="input-group-dough flex gap-l between">
-            <div className="chooseSize flex column ">
-              <label className="font-size-l padding-topbottom">
+          <div className="input-group-dough flex gap-l between container-lg">
+            <div className="chooseSize flex column">
+              <label className="font-size-l">
                 <strong>Boyut Seç *</strong>
               </label>
-              <label className="padding-topbottom">
-                <input className="padding-left" type="radio" name="size" />
-                Küçük
-              </label>
-              <label className="padding-topbottom">
-                <input type="radio" name="size" />
-                Orta
-              </label>
-              <label className="padding-topbottom">
-                <input type="radio" name="size" />
-                Büyük
-              </label>
+              {pizzaSizes.map((pizzaSize) => (
+                <div className="grid-item" key={pizzaSize.id}>
+                  <label className="flex gap-s font-weight" htmlFor="size">
+                    <input
+                      key={pizzaSize.id}
+                      type="radio"
+                      name="size"
+                      value={pizzaSize.boy}
+                    />
+                    {pizzaSize.size}
+                  </label>
+                </div>
+              ))}
             </div>
             {/* ORDER SIZE SECTION END */}
 
@@ -154,70 +131,100 @@ function App() {
             <div className="chooseDoughType flex column">
               <label
                 htmlFor="dough"
-                className="title-dough font-size-l padding-topbottom padding-bottom text-start"
+                className="title-dough font-size-l padding-bottom flex column text-start"
               >
-                <strong>Hamur Seç</strong>
-                <span className="span-dough">*</span>
+                <strong>Hamur Seç *</strong>
+                <select
+                  name="dough"
+                  id="dough"
+                  defaultValue={"-1"}
+                  // value={init.name}
+                >
+                  <option value="-1" disabled={true}>
+                    Hamur Kalınlığı Seçiniz
+                  </option>
+                  {doughTypes.map((doughType) => (
+                    <option
+                      className="semi-bol"
+                      key={doughType.id}
+                      value={doughType.size}
+                    >
+                      {doughType.size}
+                    </option>
+                  ))}
+                </select>
               </label>
-              <select
-                className="padding-topbottom"
-                name="dough"
-                id="dough"
-                defaultValue={"-1"}
-                value={init.name}
-                onChange={handleChange}
-              >
-                <option value="-1" disabled={true}>
-                  Hamur Kalınlığı Seçiniz
-                </option>
-                <option value="ince">İnce</option>
-                <option value="normal">Normal</option>
-                <option value="kalin">Kalın</option>
-              </select>
             </div>
-            {/* ORDER DOUGH TYPE SECTION END */}
           </div>
 
-          {/* EXTRA MATERIALS  SECTION START */}
+          {/* EXTRA MATERIALS  CHECKBOX START */}
           <div className="extra-materials container-lg">
-            <h2 className="padding-topbottom">Ek Malzemeler</h2>
-            <p className="p-long">En fazla 10 mazleme seçebilirsiniz. 5 TL</p>
-            <div>
+            <h2>Ek Malzemeler</h2>
+            <p>En fazla 10 malzeme seçebilirsiniz. Tanesi 5 TL</p>
+            <div className="grid-container between">
               {extras.map((extra, ind) => (
-                <Extra
-                  key={ind}
-                  onChange={handleChange}
-                  checked={formData.malzemeler.includes(extra.value)}
-                  name="malzemeler"
-                  value={extra.value}
-                  label={extra.label}
-                />
+                <div className="grid-item" key={extra.id}>
+                  <label className="flex gap-s font-weight">
+                    <input
+                      type="checkbox"
+                      onChange={addElection}
+                      value={extra.name}
+                    />
+                    {extra.name}
+                  </label>
+                </div>
               ))}
             </div>
           </div>
-          {/* EXTRA MATERIALS  SECTION END */}
+          {/* EXTRA MATERIALS  CHECKBOX END */}
         </div>
 
-        {/* ADD NOTES SECTION END */}
-        <div className="note-and-order-summary text-start">
-          <div className="add-notes">
+        {/* ADD NOTES START */}
+        {/* <div className="note-and-order-summary text-start"> */}
+        <div className="container-lg flex column margin-bottom-lg">
+          <label htmlFor="order-note">
             <h2>Sipariş Notu</h2>
-            <textarea
-              name="note"
-              id="note"
-              placeholder="Siparişinize eklemek istediğiniz bir not var mı?"
-            ></textarea>
-          </div>
-          {/* <div className="bootom-site">
+          </label>
+          <textarea
+            name="order-note"
+            id="order-note"
+            placeholder="Siparişinize eklemek istediğiniz bir not var mı?"
+            cols={5}
+            rows={4}
+          ></textarea>
+        </div>
+        {/* ADD NOTES END */}
+
+        {/* QUANTITY SELECTION START */}
+        <div className="bottom-site">
           <div className="quantity-select">
-            <button className="decrement btn" onClick={onDecrement}>
+            <button className="decrement btn" onClick={decrement}>
               -
             </button>
-            <button className="increment btn" onClick={onIncrement}>
+            <div className="counter">{count}</div>
+            <button className="increment btn" onClick={increment}>
               +
             </button>
           </div>
-        </div> */}
+        </div>
+        {/* QUANTITY SELECTION END */}
+
+        <div className="order-summary-container">
+          <div className="order summary">
+            <h3>Sipariş Toplamı</h3>
+            <div className="summary-item.election">
+              <span>Seçimler: </span>
+              {/* Seçilen malzeleme adedi * 5TL ile ekrana yazar. */}
+              <span>{elections.length * 5} TL</span>
+            </div>
+            <div className="summary-item-total">
+              <span>Toplam: </span>
+              <span>{totalPrice * count + elections.length * 5 * count}</span>
+            </div>
+            <button className="order-button btn" onClick={handleOrderCompleted}>
+              SİPARİŞ VER
+            </button>
+          </div>
         </div>
       </main>
     </div>
